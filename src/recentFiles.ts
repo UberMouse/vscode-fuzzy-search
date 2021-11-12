168bd041a8d1d2cacdff368cba1f425827eafd2e
Cassio Zen <cassio@hey.com>
2021-11-12 11:27:52 -0600

    Yet another PR with general improvements and a bugfix (#19)
    Co-authored-by: Altair J. Fernandes <con@tato.dev>

--- src/recentFiles.ts
+++ src/recentFiles.ts
@@ -13,11 +13,11 @@ export default class RecentFiles {
     this.searchResults = [];
   }
 
-  invertFilePath(filePath: string) {
+  shortenFilePath(filePath: string) {
     const parsed = path.parse(filePath);
     const workspacePath =
     vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0].uri.path;
-    return parsed.base + filePath.replace(`${workspacePath}/` || '', '');
+    return filePath.replace(`${workspacePath}` || '', '') + parsed.base;
   }
 
   addFile(filePath: string) {
@@ -26,7 +26,7 @@ export default class RecentFiles {
       this.recentFiles = this.recentFiles.filter(result => result !== existing);
       this.recentFiles.unshift(existing);
     } else {
-      this.recentFiles.unshift({key:this.invertFilePath(filePath), filePath: filePath});
+      this.recentFiles.unshift({key:this.shortenFilePath(filePath), filePath: filePath});
     }
     this.workspaceState.update('recents', this.recentFiles);
   }
